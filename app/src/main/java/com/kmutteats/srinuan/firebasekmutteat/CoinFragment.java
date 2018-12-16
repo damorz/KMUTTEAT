83d5f9e85c003119ccb422e5f32d4695b31cc2ac
damorz <dive2080@gmail.com>
2018-12-16 23:49:25 +0700

    bugg all fix except noti

--- app/src/main/java/com/kmutteats/srinuan/firebasekmutteat/CoinFragment.java
+++ app/src/main/java/com/kmutteats/srinuan/firebasekmutteat/CoinFragment.java
@@ -4,6 +4,7 @@ package com.kmutteats.srinuan.firebasekmutteat;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -32,6 +34,7 @@ public class CoinFragment extends Fragment {
 
     TextView coin;
     FirebaseFirestore db;
+    CardView addcoin;
     public CoinFragment() {
         // Required empty public constructor
     }
@@ -44,9 +47,10 @@ public class CoinFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_coin, container, false);
         // Inflate the layout for this fragment
 
+        addcoin = view.findViewById(R.id.maddCoin);
         coin = view.findViewById(R.id.bigcoin);
         SharedPreferences prefs3 = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        String email = prefs3.getString("Emailtest3", "no id");
+        final String email = prefs3.getString("Emailtest3", "no id");
         db=FirebaseFirestore.getInstance();
         db.collection("account").document("CUSTOMER").collection(email).document("data account")
                 .get()
@@ -59,15 +63,38 @@ public class CoinFragment extends Fragment {
                         }
                     }
                 });
-        /*addCoin.setOnClickListener(new View.OnClickListener() {
+        addcoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences prefs3 = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-                final String email = prefs3.getString("Emailtest3", "no id");
+                //Toast.makeText(getActivity().getApplicationContext(), "เย้ ได้ละแง้", Toast.LENGTH_SHORT).show();
+                db = FirebaseFirestore.getInstance();
+                db.collection("account").document("CUSTOMER").collection(email).document("data account")
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                String coinCus = documentSnapshot.getString("Coin") ;
+                                double coinCusInt  = Double.valueOf(coinCus);
+                                coinCusInt = coinCusInt + 100;
+                                Map<String , Object> coinCusreturn = new HashMap<>();
+                                coinCusreturn.put("Coin",""+coinCusInt);
+                                final double finalCoinCusInt = coinCusInt;
+                                db.collection("account").document("CUSTOMER").collection(email).document("data account")
+                                        .set(coinCusreturn)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                coin.setText(""+ finalCoinCusInt);
+                                            }
+                                        });
+
+                            }
+                        });
+
 
 
             }
-        });*/
+        });
 
 
         return view;
